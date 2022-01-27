319fce65f58ba8aee658b750d10c8de59740719e
elzaziedina <ziedina.elza@gmail.com>
2022-01-27 15:17:01 +0200

    Otrais

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const vardi=['Pēteris Bērziņš', 'Jānis Ozoliņš', 'Anna Krūze', 'Madara Apse', 'Kristīne Vējone']; // balvu saņēmēji, ierakstīti masīvos
-const balvas=['viedtālrunis', 'tējkanna', 'treniņbikses', 'ceļojums uz Bali', 'čipsi']; // balvas, ierakstītas masīvos
+const vardi = ['Pēteris Bērziņš', 'Jānis Ozoliņš', 'Anna Krūze', 'Madara Apse', 'Kristīne Vējone']; // balvu saņēmēji, ierakstīti masīvos
+const balvas = ['viedtālrunis', 'tējkanna', 'treniņbikses', 'ceļojums uz Bali', 'čipsi']; // balvas, ierakstītas masīvos
 
-let uzvaretajuSkaits=5; // ir pieņemts, ja ir 2 vārdi - pirmo raksta ar mazo burtu un otru ar lielo
-let rand=Math.random()*5; // tiek izmantota matemātikas izteiksme, lai izlozes kārtībā noteiktu balvu katram cilvēkam (tas*5 parāda, ka ir 5 balvas, no kā notiek izvēle)
+let uzvaretajuSkaits = 5; // ir pieņemts, ja ir 2 vārdi - pirmo raksta ar mazo burtu un otru ar lielo
+let rindas = document.querySelector('.rindas'); // tiks ievietots rindās
 
-rand=Math.floor(rand); // tiek noapaļots uz leju
+rindas.innerHTML = ' ';
 
-console.log(vardi[rand]); // izvada iegūtos rezultātus (vārds+randomā iegūto balvu) konsolē
+for (let i = 0; i < uzvaretajuSkaits; i++) {
 
+    let rand = Math.random() * 5; // tiek izmantota matemātikas izteiksme, lai izlozes kārtībā noteiktu balvu katram cilvēkam (tas*5 parāda, ka ir 5 balvas, no kā notiek izvēle)
+    rand = Math.floor(rand); // tiek noapaļots uz leju
+
+    let uzvaretajs = vardi[rand]; // izvada iegūtos rezultātus (vārds+randomā iegūto balvu) konsolē
+    rindas.innerHTML += `
+
+    <tr>
+    <td> ${i+1} </td>
+    <td> ${uzvaretajs} </td>
+    </tr>`
+
+}
